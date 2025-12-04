6118e3e2c6cc663de3af6a28dbd1e4a226c45017
SHANTO <rabbymd55555@gmail.com>
2025-12-04 18:52:22 +0600

    added private route and others

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
@@ -8,7 +9,10 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://<db_username>:<db_password>@cluster0.hy50zfn.mongodb.net/?appName=Cluster0`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.hy50zfn.mongodb.net/?appName=Cluster0`;
+
+// console.log(process.env.DB_USER);
+// console.log(process.env.DB_PASSWORD);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -23,6 +27,26 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    // jobs api
+    const jobsCollections = client.db("CareerCode").collection("jobs");
+
+    app.get("/jobs", async (req, res) => {
+      const cursor = jobsCollections.find();
+      const result = await cursor.toArray();
+      res.send(result);
+      // console.log(jobs);
+    });
+
+    app.get("/jobs/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const cursor = await jobsCollections.findOne(query);
+      // const result =  cursor.toArray();
+      res.send(cursor);
+      // console.log(jobs);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
